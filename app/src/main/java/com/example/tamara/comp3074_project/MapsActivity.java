20970d622b952a97242eb35d773d12a848e6f2bb
morgan-gill <32048628+morgan-gill@users.noreply.github.com>
2018-12-06 13:11:13 -0500

    Updating Map Activity in anticipation of incorporating directions
    Updating Map Activity in anticipation of incorporating directions

--- app/src/main/java/com/example/tamara/comp3074_project/MapsActivity.java
+++ app/src/main/java/com/example/tamara/comp3074_project/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.example.tamara.comp3074_project;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +17,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+
+    private LocationManager locationManager;
+
+    private static final int LOCATION_CODE = 1;
+
+    Polyline polyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_CODE);
+        }
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("LOCATION", "Permission not granted.");
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +84,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng gbcCasaLoma = new LatLng(43.6757061, -79.4129735);
+        mMap.addMarker(new MarkerOptions().position(gbcCasaLoma).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(gbcCasaLoma));
+        polyline = mMap.addPolyline(new PolylineOptions().add());
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if (mMap != null) {
+            LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions mark = new MarkerOptions().position(pos).title("Current Location");
+            mark.snippet("You Are Here.");
+            mMap.addMarker(mark);
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(pos));
+            // mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
