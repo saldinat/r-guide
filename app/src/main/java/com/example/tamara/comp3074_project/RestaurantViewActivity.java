46fa5d28d1a5366c59c7d159512fbbaa19e32501
morgan-gill <32048628+morgan-gill@users.noreply.github.com>
2018-12-06 20:12:58 -0500

    added email share function

--- app/src/main/java/com/example/tamara/comp3074_project/RestaurantViewActivity.java
+++ app/src/main/java/com/example/tamara/comp3074_project/RestaurantViewActivity.java
@@ -1,11 +1,16 @@
 package com.example.tamara.comp3074_project;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RestaurantViewActivity extends Activity {
+
+    String rName, rAddress, rPhone, rDescription, rTags = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,31 @@ public class RestaurantViewActivity extends Activity {
             tvDescription.setText(i.getStringExtra("description"));
             tvTags.setText(i.getStringExtra("tags"));
 
+            rName = tvName.getText().toString();
+            rAddress = tvAddress.getText().toString();
+            rPhone = tvPhone.getText().toString();
+            rDescription = tvDescription.getText().toString();
+            rTags = tvTags.getText().toString();
         }
+
+        Button btnShareEmail = findViewById(R.id.btnShareEmail);
+
+        btnShareEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String eBody = "\n" + "\n" + "\n" + rName + ":" + "\n" + rAddress + "\n" + "Phone: " + rPhone+ "\n" +
+                        rDescription + "\n" + "Tags:" + rTags;
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto","abc@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Great new restaurant");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, eBody);
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+
+            }
+        });
+
     }
 
 }
